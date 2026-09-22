643d48c9a24e7fb0ce844912ea4453c76e68deda
Sopharo BIN <sopharo.bin@khalibre.com>
2026-09-22 15:29:03 +0700

    feat: telegram broker login
    https://khalibre.atlassian.net/browse/IAM-33

---
+++ keycloak-provider/src/main/java/com/khalibre/keycloak/provider/telegram/AuthStateSession.java
@@ -0,0 +1,41 @@
+package com.khalibre.keycloak.provider.telegram;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.keycloak.models.KeycloakSession;
+
+public class AuthStateSession extends AuthStateCache {
+
+  private static final String KEY_AUTH_STATE_ID = "authStateId";
+
+  public static AuthState create(KeycloakSession session, String sessionId) {
+    AuthState authState = createEmpty();
+    setAuthStateId(session, sessionId, authState.getId());
+    return authState;
+  }
+
+  public static AuthState get(KeycloakSession session, String sessionId) {
+    return get(getAuthStateId(session, sessionId));
+  }
+
+  public static void remove(KeycloakSession session, String sessionId) {
+    remove(getAuthStateId(session, sessionId));
+  }
+
+  private static String getAuthStateId(KeycloakSession session, String sessionId) {
+    Map<String, String> notes = session.singleUseObjects().get(sessionId);
+    if (notes != null) {
+      return notes.get(KEY_AUTH_STATE_ID);
+    }
+    return null;
+  }
+
+  private static void setAuthStateId(KeycloakSession session, String sessionId, String id) {
+    Map<String, String> notes = session.singleUseObjects().get(sessionId);
+    if (notes == null) {
+      notes = new HashMap<>();
+    }
+    notes.put(KEY_AUTH_STATE_ID, id);
+    session.singleUseObjects().put(sessionId, AuthState.LIFESPAN_SECONDS, notes);
+  }
+}
